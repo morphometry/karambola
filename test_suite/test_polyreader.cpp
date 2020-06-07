90299110274ac47cb41467f7c3775aa6586fac84
Sebastian Kapfer <sebastian.kapfer@fau.de>
2020-06-07 14:43:57 +0200

    sk - Remove unused variable

--- test_suite/test_polyreader.cpp
+++ test_suite/test_polyreader.cpp
@@ -7,9 +7,7 @@ TEST_CASE( "polyreader ok" ){
     std::string infilename = "inputs/sphere.poly";
     std::cout << "testing:  " << infilename << std::endl;
     Triangulation surface;
-    CalcOptions CO;
     std::ifstream is (infilename.c_str());
-//    CO.labels_set = true;
     ReadIntoTriangulation adap1 (&surface, false);
     TriangulatingPolyFileSink adap2 (&adap1);
     parse_poly_file (&adap2, is);
@@ -20,9 +18,7 @@ TEST_CASE( "polyreader fails, poly file has no labels" ){
     std::string infilename = "inputs/sphere.poly";
     std::cout << "testing:  " << infilename << std::endl;
     Triangulation surface;
-    CalcOptions CO;
     std::ifstream is (infilename.c_str());
-//    CO.labels_set = true;
     ReadIntoTriangulation adap1 (&surface, true);
     TriangulatingPolyFileSink adap2 (&adap1);
     REQUIRE_THROWS(parse_poly_file (&adap2, is));
