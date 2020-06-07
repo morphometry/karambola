addb0c3f3b237506f95efdaf341a06ab547a6dc0
Sebastian Kapfer <sebastian.kapfer@fau.de>
2020-06-07 14:42:05 +0200

    sk - Reorder tests and fix name

--- test_suite/test_polyreader.cpp
+++ test_suite/test_polyreader.cpp
@@ -3,17 +3,6 @@
 #include "catch.hpp"
 #include <fstream>
 
-TEST_CASE( "polyreader fails" ){
-    std::string infilename = "inputs/sphere.poly";
-    std::cout << "testing:  " << infilename << std::endl;
-    Triangulation surface;
-    CalcOptions CO;
-    std::ifstream is (infilename.c_str());
-//    CO.labels_set = true;
-    ReadIntoTriangulation adap1 (&surface, true);
-    TriangulatingPolyFileSink adap2 (&adap1);
-    REQUIRE_THROWS(parse_poly_file (&adap2, is));
-}
 TEST_CASE( "polyreader ok" ){
     std::string infilename = "inputs/sphere.poly";
     std::cout << "testing:  " << infilename << std::endl;
@@ -27,3 +16,14 @@ TEST_CASE( "polyreader ok" ){
     CHECK (surface.n_triangles () == 4800);
 }
 
+TEST_CASE( "polyreader fails, poly file has no labels" ){
+    std::string infilename = "inputs/sphere.poly";
+    std::cout << "testing:  " << infilename << std::endl;
+    Triangulation surface;
+    CalcOptions CO;
+    std::ifstream is (infilename.c_str());
+//    CO.labels_set = true;
+    ReadIntoTriangulation adap1 (&surface, true);
+    TriangulatingPolyFileSink adap2 (&adap1);
+    REQUIRE_THROWS(parse_poly_file (&adap2, is));
+}
